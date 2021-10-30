0cc9996d096f5d2d49d7e661b248ea5da57a9359
Brian3647 <victoris3647@gmail.com>
2021-10-30 11:38:04 +0200

    fix: Allow use of ` and \' in commit messages safely

--- src/git.rs
+++ src/git.rs
@@ -3,7 +3,13 @@ use std::fmt::Display;
 use crate::system;
 
 pub fn commit_msg<T: ToString>(m: T) {
-	system!("git commit -m \"{}\"", m.to_string().replace('"', "\\\""))
+	system!(
+		"git commit -m \"{}\"",
+		m.to_string()
+			.replace('"', "\\\"")
+			.replace('`', "\\`")
+			.replace('\'', "\\'")
+	);
 }
 
 pub fn add<T: Display>(p: T) {
